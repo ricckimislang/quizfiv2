43a3d15f6f64813d088f4db70ca14f83268bf08e
riccki mislang <dragonmislang9@gmail.com>
2025-03-15 20:03:11 +0800

    fixed missing dbconnection

--- admin/includes/mobile-nav.php
+++ admin/includes/mobile-nav.php
@@ -1,15 +1,39 @@
 <div class="toggle-sidebar bottom-nav">
     <ul>
-        <li class="<?= getActivePage('index.php') ?>"><a href="index.php"><img src="assets/font-icons/home.png"
-                    alt="reports"></a></li>
-        <li class="<?= getActivePage('manage_quiz.php') ?>"><a href="manage_quiz.php"><img
-                    src="assets/font-icons/quiz.png" alt="quiz"></a></li>
-        <li class="<?= getActivePage('manage_rewards.php') ?>"><a href="manage_rewards.php"><img
-                    src="assets/font-icons/reward.png" alt="rewards"></a></li>
-        <li class="<?= getActivePage('manage_users.php') ?>"><a href="manage_users.php"><img
-                    src="assets/font-icons/users.png" alt="users"></a></li>
-        <li class="<?= getActivePage('manage_voucher.php') ?>"><a href="manage_voucher.php"><img
-                    src="assets/font-icons/voucher.png" alt="vouchers"></a></li>
-        <li class="<?= getActivePage('logout.php') ?>"><a href="logout.php"><i class="bx bx-log-out-circle"></i></a></li>
+        <li class="<?= getActivePage('index.php') ?>">
+            <a href="index.php">
+                <img src="assets/font-icons/home.png" alt="Dashboard">
+            </a>
+        </li>
+        <li class="<?= getActivePage('manage_quiz.php') ?>">
+            <a href="manage_quiz.php">
+                <img src="assets/font-icons/quiz.png" alt="Manage Quiz">
+            </a>
+        </li>
+        <li class="<?= getActivePage('manage_rewards.php') ?>">
+            <a href="manage_rewards.php">
+                <img src="assets/font-icons/reward.png" alt="Manage Rewards">
+            </a>
+        </li>
+        <li class="<?= getActivePage('manage_users.php') ?>">
+            <a href="manage_users.php">
+                <img src="assets/font-icons/users.png" alt="Manage Users">
+            </a>
+        </li>
+        <li class="<?= getActivePage('manage_voucher.php') ?>">
+            <a href="manage_voucher.php">
+                <img src="assets/font-icons/voucher.png" alt="Manage Vouchers">
+            </a>
+        </li>
+        <li class="<?= getActivePage('game/create_quiz.php') ?>">
+                <a href="game/create_quiz.php">
+                    <i class="bx bxs-game"></i>
+                </a>
+            </li>
+        <li class="<?= getActivePage('logout.php') ?>">
+            <a href="logout.php">
+                <i class="bx bx-log-out-circle"></i>
+            </a>
+        </li>
     </ul>
 </div>
--- admin/includes/nav-top.php
+++ admin/includes/nav-top.php
@@ -6,22 +6,49 @@
         <i class="bi bi-list toggle-sidebar-btn"></i>
     </div>
 
-
     <div class="nav-menu">
         <ul>
-            <li class="<?= getActivePage('index.php') ?>"><a href="index.php"><img src="assets/font-icons/home.png"
-                        alt=""></i>&nbsp;Home</a></li>
-            <li class="<?= getActivePage('manage_quiz.php') ?>"><a href="manage_quiz.php"><img
-                        src="assets/font-icons/quiz.png" alt="">&nbsp;&nbsp;Quiz</a></li>
-            <li class="<?= getActivePage('manage_rewards.php') ?>"><a href="manage_rewards.php"><img
-                        src="assets/font-icons/reward.png" alt="">&nbsp;Rewards</a></li>
-            <li class="<?= getActivePage('manage_users.php') ?>"><a href="manage_users.php"><img
-                        src="assets/font-icons/users.png" alt=""></i>&nbsp;Users</a></li>
-            <li class="<?= getActivePage('manage_voucher.php') ?>"><a href="manage_voucher.php"><img
-                        src="assets/font-icons/voucher.png" alt=""></i>&nbsp;Voucher</a></li>
-            <li class="<?= getActivePage('game/create_quiz.php') ?>"><a href="game/create_quiz.php"><img
-                        src="assets/font-icons/game.png" alt=""></i>&nbsp;Game</a></li>
-            <li><a href="logout.php"><i class='bx bx-log-out-circle' style="color: #4154f1"></i></a></li>
+            <li class="<?= getActivePage('index.php') ?>">
+                <a href="index.php">
+                    <img src="assets/font-icons/home.png" alt="Home icon">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li class="<?= getActivePage('manage_quiz.php') ?>">
+                <a href="manage_quiz.php">
+                    <img src="assets/font-icons/quiz.png" alt="Quiz icon">
+                    <span>Quiz</span>
+                </a>
+            </li>
+            <li class="<?= getActivePage('manage_rewards.php') ?>">
+                <a href="manage_rewards.php">
+                    <img src="assets/font-icons/reward.png" alt="Rewards icon">
+                    <span>Rewards</span>
+                </a>
+            </li>
+            <li class="<?= getActivePage('manage_users.php') ?>">
+                <a href="manage_users.php">
+                    <img src="assets/font-icons/users.png" alt="Users icon">
+                    <span>Users</span>
+                </a>
+            </li>
+            <li class="<?= getActivePage('manage_voucher.php') ?>">
+                <a href="manage_voucher.php">
+                    <img src="assets/font-icons/voucher.png" alt="Voucher icon">
+                    <span>Voucher</span>
+                </a>
+            </li>
+            <li class="<?= getActivePage('game/create_quiz.php') ?>">
+                <a href="game/create_quiz.php">
+                    <i class="bx bxs-game"></i>
+                    <span>Game</span>
+                </a>
+            </li>
+            <li>
+                <a href="logout.php">
+                    <i class="bx bx-log-out-circle" style="color: #4154f1"></i>
+                </a>
+            </li>
         </ul>
     </div>
-</div><!-- End Logo -->
+</div>
